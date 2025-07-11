7ff6f58f1b5870259491a38d8e6e9159562020de
pioreklolek <piotrorlowski2004@gmail.com>
2025-07-11 03:08:41 +0200

    rozbudowa 2 3 4 v3

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -25,37 +25,64 @@ public class SecurityConfig {
         this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
 
-                        //pub
-                        .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/api/vehicles", "/api/vehicles/available").permitAll()
+                        // Pub
+                        .requestMatchers("/api/auth/login", "/api/auth/register").permitAll()
+                        .requestMatchers("/api/vehicles/available").permitAll()
                         .requestMatchers("/api/payments/success").permitAll()
                         .requestMatchers("/api/payments/cancel").permitAll()
                         .requestMatchers("/api/payments/webhook").permitAll()
 
-                        //admin
-                        .requestMatchers("/api/vehicles/create","/api/vehicles/delete/").hasAuthority("admin")
-                        .requestMatchers("/api/vehicles/history/**", "/api/vehicles/**","/api/vehicles/active","/api/vehicles/deleted").hasAuthority("admin")
 
-                        //user
-                        .requestMatchers("/api/rentals/rent/**","/api/rentals/return/**").hasAuthority("user")
+                        .requestMatchers("/api/auth/profile").authenticated()
+
+                        // user wlasne dane
+                        .requestMatchers("/api/rentals/history/my").hasAuthority("user")
+                        .requestMatchers("/api/rentals/active/my").hasAuthority("user")
+                        .requestMatchers("/api/rentals/rent/**").hasAuthority("user")
+                        .requestMatchers("/api/rentals/return/**").hasAuthority("user")
+
+                        // platnosc
+                        .requestMatchers("/api/payments/create-payment-intent").authenticated()
+                        .requestMatchers("/api/payments/create-checkout-session").authenticated()
+                        .requestMatchers("/api/payments/check-payment-status").authenticated()
+                        .requestMatchers("/api/payments/update-status/**").authenticated()
+
+                        //lokalizacja dla usera
+                        .requestMatchers("/api/locations/set-random-location/**").hasAnyAuthority("admin", "user")
+                        .requestMatchers("/api/locations/status/**").hasAnyAuthority("admin", "user")
+                        .requestMatchers("/api/locations/set-location/**").hasAnyAuthority("admin", "user")
+
+                        // admin
+                        .requestMatchers("/api/vehicles/create", "/api/vehicles/delete/**").hasAuthority("admin")
+                        .requestMatchers("/api/vehicles", "/api/vehicles/**", "/api/vehicles/active", "/api/vehicles/deleted").hasAuthority("admin")
+                        .requestMatchers("/api/rentals", "/api/rentals/history", "/api/rentals/history/vehicle/**").hasAuthority("admin")
                         .requestMatchers("/api/rentals/history/user/**").hasAnyAuthority("admin", "user")
-                        .requestMatchers("/api/rentals", "/api/rentals/history", "/api/rentals/history/vehicle").hasAuthority("admin")
+                        .requestMatchers("/api/users/**").hasAuthority("admin")
+                        .requestMatchers("/api/roles/**").hasAuthority("admin")
+                        .requestMatchers("/api/locations/allowed/**").hasAuthority("admin")
+                        .requestMatchers("/api/locations/not-allowed").hasAuthority("admin")
+                        .requestMatchers("/api/locations/update-all-status").hasAuthority("admin")
+                        .requestMatchers("/api/locations/set-location-by-coords/**").hasAuthority("admin")
+
                         .anyRequest().authenticated())
                 .exceptionHandling(ex -> ex.authenticationEntryPoint(jwtAuthenticationEntryPoint))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder();
     }
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
